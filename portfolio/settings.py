071400224cc648c30b6724369b79521416e20c04
nemixu <s.r.seagrave@gmail.com>
2020-09-23 16:24:53 +0100

    added css page, included scripts and head links to stylesheet, updated settings for media files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -121,3 +122,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
