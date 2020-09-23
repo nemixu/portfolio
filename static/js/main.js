6945cb3fb963513b09919481264662c4f1fc629f
nemixu <s.r.seagrave@gmail.com>
2020-09-23 23:48:50 +0100

    added jquery to handle scroll effect of navbar, and styling to match

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,10 @@
 // Scroll Element to change the transparency of the navbar at scroll position
 $(window).scroll(function() {
-    if($(this).scrollTop() < 100) {
+    if($(this).scrollTop() > 50) {
         $('.nav-main').addClass('nav-fade');
+        $('.nav-main-list-items a').addClass('text-black');
     } else {
         $('.nav-main').removeClass('nav-fade');
+        $('.nav-main-list-items a').removeClass('text-black');
     }
 });
