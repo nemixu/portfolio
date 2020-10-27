78164b466f9d46f7d4cac377808e65a716e1b8ed
nemixu <s.r.seagrave@gmail.com>
2020-10-27 22:54:35 +0000

    added urls to allow project urls, added get request from github api to pull repo information

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
+import requests
+
 # Create your views here.
 def projects(request):
     """
     View to pull projects from github api and display accordingly
     """
+    repo = "milestone4"
+    url = f"https://api.github.com/repos/nemixu/{repo}/branches/master"
+    payload = {}
+    headers = {} 
+    
+    response = requests.request('GET', url, headers=headers, data = payload)
+    
+    print(response.text.encode('utf8'))
+    return render (request, 'projects.html')
+
+===>
