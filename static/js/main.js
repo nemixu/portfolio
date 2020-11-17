066040ece00b5deaccccb7cffae3f68348c642db
nemixu <s.r.seagrave@gmail.com>
2020-11-17 13:41:41 +0000

    added variable for top-button to add animations

--- static/js/main.js
+++ static/js/main.js
@@ -12,12 +12,18 @@ $(window).scroll(function() {
 
 //Sidebar slide in effect to showcase social links and cv
 let sideBar = document.getElementById('side-bar-main');
+let toTop = document.getElementById('top-button');
 
 window.addEventListener('scroll', () => {
     if(window.scrollY > 700){
         sideBar.classList.add('slide-in');
+        toTop.classList.add('slide-in');
     }
     else if (window.scrollY <600) {
          sideBar.classList.remove('slide-in');
+         toTop.classList.remove('slide-in');
     } 
 })
+
+
+
