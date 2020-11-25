cfc76559e30c4bce1c06d718b2b1b28cbbde92a0
nemixu <s.r.seagrave@gmail.com>
2020-11-25 23:14:13 +0000

    adding unstaged files

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 import requests
+import json
 
 # Create your views here.
 def projects(request):
@@ -8,11 +9,14 @@ def projects(request):
     View to pull projects from github api and display accordingly
     """
     repo = "milestone4"
-    url = f"https://api.github.com/repos/nemixu/{repo}/branches/master"
+    response = requests.get(f"https://api.github.com/repos/nemixu/{repo}/branches/master")
+    
+    # url = f"https://api.github.com/repos/nemixu/{repo}/branches/master"
     payload = {}
     headers = {} 
     
-    response = requests.request('GET', url, headers=headers, data = payload)
+     
+    response = requests.request('GET', response, headers=headers, data = payload)
     
     print(response.text.encode('utf8'))
     return render (request, 'projects.html')
