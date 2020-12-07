eec47176e8e8e542f582c9a2a5674d2cc7dfd21c
nemixu <s.r.seagrave@gmail.com>
2020-12-07 17:58:37 +0000

    created new secret key for production, changed debug to dev.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yv+rkie2+tq8dzgy%v^(!pm^*fctmvorz-_@bio9n16(ztfl%w'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['stevo-portfolio.heroku.com', '127.0.0.1', 'seagrave.dev']
 
